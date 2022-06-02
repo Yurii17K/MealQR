2a90b326e35442f676367b88c5d796745944b2b2
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2022-06-02 11:32:18 +0200

    Added more tests to DishOpinionService

--- src/test/java/com/example/mealqr/DishOpinionServiceTests.java
+++ src/test/java/com/example/mealqr/DishOpinionServiceTests.java
@@ -1,5 +1,7 @@
 package com.example.mealqr;
+import com.example.mealqr.pojos.CustomerAllergy;
 import com.example.mealqr.pojos.Dish;
+import com.example.mealqr.pojos.DishComment;
 import com.example.mealqr.pojos.DishRating;
 import com.example.mealqr.preferenceAnalysis.SlopeOne;
 import com.example.mealqr.repositories.*;
@@ -8,9 +10,11 @@ import io.vavr.Tuple;
 import io.vavr.Tuple2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.math.BigDecimal;
@@ -23,22 +27,35 @@ import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-class DishOpinionServiceTests {
+public class DishOpinionServiceTests {
     
     @Mock
     private  DishRatingRepository dishRatingRepository;
     @Mock
     private  DishRepository dishRepository;
     @Mock
+    private  UserRepository userRepository;
+
+    @Mock
+    private DishCommentRepository dishCommentRepository;
+    @Mock
     private  RestaurantEmployeeRepository restaurantEmployeeRepository;
     @Mock
     private  SlopeOne slopeOne;
+
+    @Mock
+    private CustomerAllergyRepository customerAllergyRepository;
 
     private static final String SUCH_DISH_DOES_NOT_EXIST = "Such dish does not exist";
 
     @InjectMocks
     private DishOpinionService dishOpinionService;
-    
+
+    @BeforeEach
+    public void setup(){
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     public void shouldReturnEmptyDishes() {
         
@@ -64,17 +81,49 @@ class DishOpinionServiceTests {
         testMap.put(1,2.0);
         String testRestaurantName = "Test restaurant";
         String testUserEmail = "TestUser@email.com";
-
+        List<String> userEmailList = new ArrayList();
+        userEmailList.add(testUserEmail);
+        userEmailList.add("test@test.com");
         when(restaurantEmployeeRepository.existsByRestaurantName(testRestaurantName)).thenReturn(true);
         when(dishRepository.findAllByRestaurantName(testRestaurantName)).thenReturn(testList);
         when(slopeOne.slopeOne(eq(testUserEmail),any())).thenReturn(testMap);
 
+        when(userRepository.findAllCustomersWhoRatedAnythingInRestaurant(anyString())).thenReturn(userEmailList);
         //when
         List<Tuple2<Dish, Tuple2<Double, List<String>>>> result = dishOpinionService.getAllDishesInRestaurantConfiguredForUser(testUserEmail, testRestaurantName);
 
         //then
         assertFalse(result.isEmpty());
     }
+
+    @Test
+    public void shouldReturnListWithoutKiwi(){
+
+        //given
+        List<Dish> testList= new ArrayList<>();
+        Dish badDish = new Dish(1,"Kiwi kiwi","test","test".getBytes(StandardCharsets.UTF_8), BigDecimal.TEN,"Kiwi kiwi kiwi kiwi");
+        Dish goodDish = Dish.builder().ID(2).dishName("Good dish").restaurantName("test").dishImg("test".getBytes(StandardCharsets.UTF_8)).dishPrice(BigDecimal.TEN).dishDescription("This dish is very good").build();
+        testList.add(badDish);
+        testList.add(goodDish);
+        Map<Integer, Double> testMap= new HashMap<>();
+        testMap.put(1,2.0);
+        String testRestaurantName = "Test restaurant";
+        String testUserEmail = "TestUser@email.com";
+        CustomerAllergy testAllergy = CustomerAllergy.builder().allergies("kiwi").userEmail(testUserEmail).build();
+
+        when(restaurantEmployeeRepository.existsByRestaurantName(testRestaurantName)).thenReturn(true);
+        when(dishRepository.findAllByRestaurantName(testRestaurantName)).thenReturn(testList);
+        when(slopeOne.slopeOne(eq(testUserEmail),any())).thenReturn(testMap);
+        when(customerAllergyRepository.findByUserEmail(anyString())).thenReturn(Optional.of(testAllergy));
+        //when
+        List<Tuple2<Dish, Tuple2<Double, List<String>>>> result = dishOpinionService.getAllDishesInRestaurantConfiguredForUser(testUserEmail, testRestaurantName);
+
+        //then
+        for(Tuple2<Dish, Tuple2<Double, List<String>>> element : result){
+            assertFalse(element._1.getDishDescription().toLowerCase(Locale.ROOT).contains("kiwi"));
+        }
+    }
+
 
     @Test
     public void shouldRejectDishRatingDishDoesNotExist(){
@@ -142,7 +191,7 @@ class DishOpinionServiceTests {
     @Test
     public void shouldAddComment() {
 
-        // given
+        
         String testRestaurantName = "Test restaurant";
         String testUserEmail = "TestUser@email.com";
         String testDishName = "Test dish";
@@ -150,12 +199,28 @@ class DishOpinionServiceTests {
 
         Dish testDish = new Dish(1, "Test", "test", "test".getBytes(StandardCharsets.UTF_8), BigDecimal.TEN, "test");
 
-        // when
+        
         when(dishRepository.findByDishNameAndRestaurantName(anyString(), anyString())).thenReturn(Optional.of(testDish));
-        when(dishRatingRepository.findByDishIdAndUserEmail(any(), anyString())).thenReturn(Optional.empty());
-
-        // then
+        when(dishCommentRepository.findByDishIdAndUserEmail(anyInt(),anyString())).thenReturn(Optional.empty());
+        
         assertEquals(Tuple.of(true, "Added comment to dish " + testDishName + " from " + testRestaurantName), dishOpinionService.addOrUpdateComment(testUserEmail, testDishName, testRestaurantName, testComment));
     }
+    @Test
+    public void shouldUpdateComment() {
 
+        
+        String testRestaurantName = "Test restaurant";
+        String testUserEmail = "TestUser@email.com";
+        String testDishName = "Test dish";
+        String testComment = "Test comment";
+
+        Dish testDish = new Dish(1, "Test", "test", "test".getBytes(StandardCharsets.UTF_8), BigDecimal.TEN, "test");
+        DishComment testDishComment = DishComment.builder().comment("comment").dishId(1).userEmail(testUserEmail).ID(1).build();
+        
+        when(dishRepository.findByDishNameAndRestaurantName(anyString(), anyString())).thenReturn(Optional.of(testDish));
+        when(dishCommentRepository.findByDishIdAndUserEmail(anyInt(),anyString())).thenReturn(Optional.of(testDishComment));
+
+        
+        assertEquals(Tuple.of(true, "Updated comment to dish " + testDishName + " from " + testRestaurantName), dishOpinionService.addOrUpdateComment(testUserEmail, testDishName, testRestaurantName, testComment));
+    }
 }
