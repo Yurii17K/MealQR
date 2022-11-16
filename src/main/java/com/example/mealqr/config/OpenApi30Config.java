f5db80fb727bf378c7e44f87717f92f878b90cce
Yurii17K <yura11231@gmail.com>
2022-11-16 23:48:57 +0100

    'Try it out' button removed from OpenApi
    - no need to click try it out every time one wants to test an endpoint

--- src/main/java/com/example/mealqr/config/OpenApi30Config.java
+++ src/main/java/com/example/mealqr/config/OpenApi30Config.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
