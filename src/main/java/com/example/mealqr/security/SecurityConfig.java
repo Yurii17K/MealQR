fcb5d4550bc0527da8c4d7de83cfdb76678a4207
Yurii17K <yura11231@gmail.com>
2022-11-22 10:36:38 +0100

    CORS

--- src/main/java/com/example/mealqr/security/SecurityConfig.java
+++ src/main/java/com/example/mealqr/security/SecurityConfig.java
@@ -73,5 +73,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
-
 }
