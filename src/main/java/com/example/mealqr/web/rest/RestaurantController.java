3302905126f0c6eb00a7b0388e4f43113b6384a1
Yurii17K <yura11231@gmail.com>
2022-12-03 10:20:41 +0100

    Update RestaurantController.java

--- src/main/java/com/example/mealqr/web/rest/RestaurantController.java
+++ src/main/java/com/example/mealqr/web/rest/RestaurantController.java
@@ -1,5 +1,6 @@
 package com.example.mealqr.web.rest;
 
+import com.example.mealqr.domain.enums.Roles;
 import com.example.mealqr.services.RestaurantService;
 import com.example.mealqr.web.rest.reponse.RestaurantRes;
 import com.example.mealqr.web.rest.request.RestaurantSaveReq;
@@ -7,12 +8,16 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -26,5 +31,20 @@ public class RestaurantController {
     public ResponseEntity<RestaurantRes> createRestaurant(Principal principal,
             @RequestBody RestaurantSaveReq restaurantSaveReq) {
         return ResponseEntity.ok(restaurantService.createRestaurant(principal.getName(), restaurantSaveReq));
+    }
+
+    @GetMapping("/restaurants")
+    @Operation(summary = "Get all restaurants that belong to the current user", security = @SecurityRequirement(name = "JWT AUTH"))
+    public ResponseEntity<List<String>> createRestaurant(Authentication authentication) {
+        Set<SimpleGrantedAuthority> authoritiesToRemove = Arrays.stream(Roles.values())//
+                .map(Enum::name)//
+                .map(SimpleGrantedAuthority::new)//
+                .collect(Collectors.toSet());
+        authoritiesToRemove.add(new SimpleGrantedAuthority(authentication.getName()));
+        return ResponseEntity.ok(authentication.getAuthorities()//
+                .stream()//
+                .filter(grantedAuthority -> !authoritiesToRemove.contains(grantedAuthority))//
+                .map(GrantedAuthority::getAuthority)//
+                .collect(Collectors.toList()));
     }
 }
